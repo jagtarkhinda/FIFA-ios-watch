61c4e412ca6823aee3fe4901411a6a57e6eb9729
Jagtar Singh <jagtarsingh@Jagtars-Mac.local>
2019-07-01 14:26:32 -0700

    first commit

---
+++ FifaGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FifaGames
+//
+//  Created by Jagtar Singh on 2019-07-01.
+//  Copyright © 2019 Jagtar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
